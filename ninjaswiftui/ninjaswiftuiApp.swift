f4461058982f4c6d6d74660e6b8a4a499190609f
William Smith <drwillsmith@gmail.com>
2022-01-23 15:40:28 -0600

    Initial Commit

---
+++ ninjaswiftui/ninjaswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ninjaswiftuiApp.swift
+//  ninjaswiftui
+//
+//  Created by William Smith on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ninjaswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
