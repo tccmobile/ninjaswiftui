b0c1bd2eaf22461bec2fefa305369af430fdd47f
William Smith <drwillsmith@gmail.com>
2022-01-26 17:34:20 -0600

    Ready

---
+++ ninjaswiftui/GameOverScene.swift
@@ -0,0 +1,12 @@
+//
+//  GameOverScene.swift
+//  ninjaswiftui
+//
+//  Created by William Smith on 1/26/22.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+}
--- ninjaswiftui/GameScene.swift
+++ ninjaswiftui/GameScene.swift
@@ -7,7 +7,45 @@
 
 import SwiftUI
 import SpriteKit
+import Foundation
+
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+func sqrt(a: CGFloat) -> CGFloat {
+  return CGFloat(sqrtf(Float(a)))
+}
+#endif
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
+  }
+  
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+}
 
 class GameScene: SKScene {
-   
+    struct PhysicsCategory {
+        static let none      : UInt32 = 0
+        static let all       : UInt32 = UInt32.max
+        static let monster   : UInt32 = 0b1
+        static let projectile: UInt32 = 0b10      
+      }
 }
