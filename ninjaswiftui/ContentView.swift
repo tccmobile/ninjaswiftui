a78b32c11d97bf1c4ed5d559034c4e3d2cde4c9b
tccmobile <William.smith@tulsacc.edu>
2022-02-01 13:40:59 -0600

    Week 1 done

--- ninjaswiftui/ContentView.swift
+++ ninjaswiftui/ContentView.swift
@@ -14,15 +14,17 @@ import SpriteKit
 struct ContentView: View {
     var scene: SKScene {
         let scene = GameScene()
+
         scene.size = CGSize(width: 300, height: 400)
         scene.scaleMode = .aspectFill
+        
         return scene
     }
 
     var body: some View {
         SpriteView(scene: scene)
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-
+            
             .ignoresSafeArea()
     }
 }
--- ninjaswiftui/GameScene.swift
+++ ninjaswiftui/GameScene.swift
@@ -5,9 +5,8 @@
 //  Created by William Smith on 1/26/22.
 //
 
-import SwiftUI
 import SpriteKit
-import Foundation
+
 
 func +(left: CGPoint, right: CGPoint) -> CGPoint {
   return CGPoint(x: left.x + right.x, y: left.y + right.y)
@@ -41,11 +40,47 @@ extension CGPoint {
   }
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     struct PhysicsCategory {
         static let none      : UInt32 = 0
         static let all       : UInt32 = UInt32.max
         static let monster   : UInt32 = 0b1
         static let projectile: UInt32 = 0b10      
       }
+    
+    let player = SKSpriteNode(imageNamed:  "player")
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.white
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        addChild(player)
+        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+        
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random())/0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addMonster(){
+        let monster = SKSpriteNode(imageNamed: "monster")
+        
+        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
+        monster.physicsBody?.isDynamic = true
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.monster
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.projectile
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.none
+        
+        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
+        
+        addChild(monster)
+    }
 }
